879ce5f725143f084f7410d2984940420e1020fb
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-03 03:14:41 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,4 +1,8 @@
 import os 
+
+import subprocess
+
+
 counter = 0
 print("If you want all the excel file, for example write .xlsx")
 inp = ".py"
@@ -8,5 +12,7 @@ for r, d, f in os.walk("/home/runner/work/"): # change the hard drive, if you wa
         filepath = os.path.join(r, file)
         if inp in file:
         	counter += 1
+            FilePath = os.path.join(r, file)
+            subprocess.run(["optimize-images", FilePath])
         	print(os.path.join(r, file))
-print(f"trovati {counter} files.")
+print(f"Optimized {counter} files.")
