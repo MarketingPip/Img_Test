a235c3c7e8b61b95041737898aa7bad4a2f5c81f
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-03 02:59:27 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,7 +1,7 @@
  
 counter = 0
 print("If you want all the excel file, for example write .xlsx")
-inp = input("What are you looking for?:> ")
+inp = ".py"
 thisdir = os.getcwd()
 for r, d, f in os.walk("/home/runner/work/"): # change the hard drive, if you want
     for file in f:
